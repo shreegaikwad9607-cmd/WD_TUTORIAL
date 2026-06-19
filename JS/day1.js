499b20e2c029f42b7ca2935b2a7f48c82f7af571
Amit Gaikwad <shreegaikwad9607@gmail.com>
2026-06-19 20:41:19 +0530

    css an js

---
+++ JS/day1.js
@@ -0,0 +1,8 @@
+        console.log("This is console");
+        alert("This is alert")
+        confirm("this is confirm")
+        document.writeln("This is new document")
+        document.write("This is Document")
+        // input method
+        let a= prompt("ENTER YOUR NAME :")
+        document.writeln("your name is ",a)
---
+++ JS/string.js
@@ -0,0 +1,83 @@
+let a="Fortune"
+console.log(a)
+console.log(typeof(a));
+
+let b=a.charAt(3);
+console.log(b);
+
+let c=a.charCodeAt(3)
+console.log(c);
+
+let d=a.endsWith('e')
+console.log(d);
+
+let e=a.startsWith('f')
+console.log(e);
+
+let f=a.includes('z')
+console.log(f);
+
+let g=a.toLocaleUpperCase()
+console.log(g);
+
+let h=a.toLocaleLowerCase()
+console.log(h);
+
+let i=a.concat("Cloud")
+console.log(i);
+
+let j=i.replace("Fortune","Cravita")
+console.log(j);
+
+let k=a.repeat(5)
+console.log(k);
+
+// number method
+// 11 11.79
+let ab=34;
+console.log(ab);
+console.log(typeof(ab));
+
+
+let xy=10.09028769
+console.log(xy);
+console.log(typeof(xy));
+
+let yz=xy.toFixed(3)
+console.log(yz);
+
+let uv=ab.toString()
+console.log(uv);
+
+console.log(typeof(uv));
+
+// boolean
+
+let n=true
+console.log(typeof(n));
+
+
+let z=null
+console.log(typeof(z));
+
+let q;
+console.log(typeof(q));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
